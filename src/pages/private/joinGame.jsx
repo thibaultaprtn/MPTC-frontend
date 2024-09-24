4d1911170ee9885c00e37c4da6e6feb9fe9c3836
Thibault APRETNA <thibaultapretna@Thibaults-MacBook-Air.local>
2024-09-24 15:48:20 +0200

    Début de la page permettant de rejoindre une partie / Correction de la db permettant de populate les users dans le dashboard et dans la page rejoindre une équipe

--- src/pages/private/joinGame.jsx
+++ src/pages/private/joinGame.jsx
@@ -3,29 +3,54 @@ import { GlobalContext } from "../../context/GlobalContext";
 import axios from "axios";
 
 const JoinGame = () => {
-  const { mongoUserId, token, user } = useContext(GlobalContext);
+  const { userMongoId, token, user } = useContext(GlobalContext);
   const [gameId, setGameId] = useState("");
   const [gameDetails, setGameDetails] = useState({});
   const [firstStep, setFirstStep] = useState(true);
   const handleFirstSubmit = async (event) => {
     event.preventDefault();
-    const response = await axios.get(
-      `${import.meta.env.VITE_BACKURL}/game/joinable`,
-      {
-        params: {
-          game_id: gameId,
-        },
-        headers: {
-          Authorization: "Bearer " + token,
-          email: user.email || "",
-        },
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKURL}/game/joinable`,
+        {
+          params: {
+            game_id: gameId,
+            user_id: userMongoId,
+          },
+          headers: {
+            Authorization: "Bearer " + token,
+            email: user.email || "",
+          },
+        }
+      );
+      console.log(response.data);
+      if (response.data) {
+        setGameDetails(response.data);
+        setFirstStep(false);
       }
-    );
-    console.log(response);
-    if (response.data.game) {
-      setGameDetails(response.data.game);
-      setFirstStep(false);
+    } catch (error) {
+      alert(error.response.data.message);
     }
+  };
+
+  const handleTeamSubmit = async (event, number) => {
+    // event.preventDefault();
+    try {
+      console.log(number);
+      // const response = await axios.put(
+      //   `${import.meta.env.VITE_BACKURL}/game/addplayer`,
+      //   {
+      //     params: {
+      //       team_number: number,
+      //       user_id: userMongoId,
+      //     },
+      //     headers: {
+      //       Authorization: "Bearer " + token,
+      //       email: user.email || "",
+      //     },
+      //   }
+      // );
+    } catch (error) {}
   };
 
   const handleGameIdChange = (event) => setGameId(event.target.value);
@@ -43,6 +68,35 @@ const JoinGame = () => {
           </label>
           <button type="submit">Rejoindre la partie</button>
         </form>
+      )}
+      {!firstStep && (
+        <div>
+          <h2>Rejoindre la partie "{gameDetails.game_name}"</h2>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            {gameDetails.team.map((elem, index) => {
+              return (
+                <div
+                  key={elem.team_number}
+                  style={{ display: "flex", flexDirection: "column" }}
+                >
+                  <button
+                    onClick={(event) =>
+                      handleTeamSubmit(event, elem.team_number)
+                    }
+                  >
+                    Equipe n° {elem.team_number}
+                  </button>
+                  <p>{elem.team_name}</p>
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    {elem.users.map((elem, index) => {
+                      return <p key={elem._id}>{elem.username}</p>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       )}
     </>
   );
